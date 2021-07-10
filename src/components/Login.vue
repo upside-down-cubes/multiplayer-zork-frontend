ba0dd92fc8d5fe56a2b63d6c86430fa0540c69ed
Krittin M1 <phupha400@hotmail.com>
2021-07-10 21:51:50 +0700

    Insert text field for user and password and button

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,29 @@
-<template>
-  <v-container v-model="$vuetify.theme.dark">
+<template v-model="$vuetify.theme.dark">
+  <v-container>
     <div class="text-center">
       <h1 class="font-weight-light">Login Page</h1>
+      <br>
     </div>
+    <v-row>
+      <v-col offset="3" cols="10" sm="4" md="6">
+        <v-text-field
+          v-model="username"
+          label="Enter Username"
+          filled
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="password"
+          label="Enter Password"
+          filled
+        ></v-text-field>
+        <v-btn
+            color="primary"
+            elevation="5"
+            @click="sendUserInfo"
+        >Login</v-btn>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -11,7 +32,13 @@ export default {
   name: "Login",
 
   data: () => ({
-   //
+    username: "",
+    password: "",
   }),
+  methods: {
+    sendUserInfo(){
+      // send user input to vertify in database
+    }
+  }
 };
 </script>
